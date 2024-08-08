5f23578651a444bc62aaeaea916ed263a550b519
kaualira-py <kauasantoslira9@gmail.com>
2024-08-08 16:25:41 -0300

    Create aula 9 questao1.py

---
+++ aula 9 questao1.py
@@ -0,0 +1,10 @@
+a = int(input("Digite um numero inteiro: "))
+b = int(input("Digite outro numero inteiro: "))
+ç = 0
+if a < b:
+    for x in range(a,b):
+        ç += x
+    print(ç)
+else:
+    print("Erro!")
+    #ç=çoma
