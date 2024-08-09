14b25875f85967b932090db4e886ebf810c38afc
kaualira-py <kauasantoslira9@gmail.com>
2024-08-09 17:06:09 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,14 +1,18 @@
-a = b = c = d = 0
-mn = ''
-nm = input("nome: ")
-d = float(input("preçu: "))
-c = d 
-for x in range(1,5):
-    nm = input("nome: ")
-    d = float(input("preçu: "))
-    a += d
-    b += 1
-    if d < c:
-        d = c
-        nm = mn
-print(f"Média {a/b}, nome do menor{mn}, preço do menor{c}")
+soma = 0
+medicamento = input("Medicamento: ")
+preco       = float(input("R$: "))
+mais_barato = medicamento
+menor_preco = preco
+soma += preco
+
+for x in range(4):
+   medicamento = input("Medicamento: ")
+   preco       = float(input("R$: "))
+   if preco < menor_preco:
+      menor_preco = preco
+      mais_barato = medicamento
+   soma += preco
+
+media = soma / 5
+print(f"{mais_barato} é o medicamento mais barato e custa R$ {menor_preco:.2f}.")
+print(f"Média dos preços: R$ {media:.2f}.")
